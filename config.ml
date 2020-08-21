7dc55eead9efb2b472a136a718b171faee3f29d2
Kevin Schoon <kevinschoon@gmail.com>
2020-08-21 12:55:32 -0500

    resolve editor from environment + config file

--- config.ml
+++ config.ml
@@ -1,6 +1,6 @@
 open Core
 
-type t = { state_dir : string; lock_file : string }
+type t = { state_dir : string; lock_file : string; editor : string option }
 
 let default_path =
   Filename.concat (Sys.home_directory ()) ".config/note/config.yaml"
@@ -10,40 +10,43 @@ let default_config =
   {
     state_dir = Filename.concat home_dir ".local/share/note";
     lock_file = Filename.concat home_dir ".local/share/note.lock";
+    editor = None;
   }
 
 let to_string config =
+  let editor = match config.editor with Some name -> name | None -> "" in
   let dict =
     Ezjsonm.dict
       [
         ("state_dir", Ezjsonm.string config.state_dir);
         ("lock_file", Ezjsonm.string config.lock_file);
+        ("editor", Ezjsonm.string editor);
       ]
   in
   Yaml.to_string_exn dict
 
 let of_string config_str =
-  let get_string_key dict name =
-    let title = List.find ~f:(fun (key, _) -> equal_string key name) dict in
-    match title with Some (_, v) -> Ezjsonm.get_string v | None -> ""
-  in
   let value = Yaml.of_string_exn config_str in
-  let dict = Ezjsonm.get_dict value in
-  let state_dir = get_string_key dict "state_dir" in
-  let lock_file = get_string_key dict "lock_file" in
-  { state_dir; lock_file }
+  let state_dir = Ezjsonm.get_string (Ezjsonm.find value [ "state_dir" ]) in
+  let lock_file = Ezjsonm.get_string (Ezjsonm.find value [ "lock_file" ]) in
+  let editor =
+    if Ezjsonm.mem value [ "editor" ] then
+      Some (Ezjsonm.get_string (Ezjsonm.find value [ "editor" ]))
+    else None
+  in
+  { state_dir; lock_file; editor }
 
-let get config key = 
-    match key with
-    | "state_dir" -> Some config.state_dir
-    | "lock_file" -> Some config.lock_file
-    | _ -> None
+let get config key =
+  match key with
+  | "state_dir" -> Some config.state_dir
+  | "lock_file" -> Some config.lock_file
+  | _ -> None
 
 let get_exn config key =
-    let result = get config key in
-    match result with
-    | Some value -> value
-    | None -> failwith (sprintf "bad configuration key: %s" key) 
+  let result = get config key in
+  match result with
+  | Some value -> value
+  | None -> failwith (sprintf "bad configuration key: %s" key)
 
 let initialize path config =
   (* ensure the directory exists *)
@@ -67,9 +70,15 @@ let initialize path config =
   | `No | `Unknown -> Unix.mkdir_p config.state_dir );
   ()
 
+let resolve config =
+    let editor = match config.editor with
+    | Some name -> Some name
+    | None -> (Sys.getenv "NOTE_EDITOR") in
+    { state_dir = config.state_dir ; lock_file = config.lock_file; editor = editor }
+
 let read_config path =
   match Sys.file_exists path with
   | `Yes ->
       let config_str = In_channel.read_all path in
-      of_string config_str
-  | `No | `Unknown -> default_config
+      (resolve (of_string config_str))
+  | `No | `Unknown -> (resolve default_config)
