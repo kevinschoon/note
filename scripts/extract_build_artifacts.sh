f2f5cb0e3517fb836465d84b6aa15da4443b8e60
Kevin Schoon <kevinschoon@gmail.com>
2020-09-01 19:12:27 -0500

    add basic script to extract linux ELFs

---
+++ scripts/extract_build_artifacts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# TODO: Capture more build specifics
+
+musl_container_id="$(docker create note-alpine)"
+glibc_container_id="$(docker create note-debian)"
+
+docker cp "$glibc_container_id:/usr/bin/note" note-glibc-latest
+docker rm "$glibc_container_id" 1>/dev/null
+docker cp "$musl_container_id:/usr/bin/note" note-musl-latest
+docker rm "$musl_container_id" 1>/dev/null
