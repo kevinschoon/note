a5f36252b123c1b6bd8445ce7fbbe2bfe3f6f3cf
Kevin Schoon <kevinschoon@gmail.com>
2020-09-03 10:54:34 -0500

    revert note back to record

--- lib/note.ml
+++ lib/note.ml
@@ -1,27 +1,30 @@
 open Core
 open Stdio
 
-type t = Ezjsonm.t * Omd.t
+type t = {
+    frontmatter: Ezjsonm.t ;
+    markdown : Omd.t ;
+}
 
 let build ?(tags = []) ?(content = "") title =
   let frontmatter =
     Ezjsonm.dict
       [ ("title", Ezjsonm.string title); ("tags", Ezjsonm.strings tags) ]
   in
-  (frontmatter, Omd.of_string content)
+  let markdown = Omd.of_string content in
+  {frontmatter; markdown}
 
 let get_title t =
-  match Ezjsonm.find_opt (Ezjsonm.value (fst t)) [ "title" ] with
+  match Ezjsonm.find_opt (Ezjsonm.value (t.frontmatter)) [ "title" ] with
   | Some v -> Ezjsonm.get_string v
   | None -> ""
 
 let get_tags t =
-  match Ezjsonm.find_opt (Ezjsonm.value (fst t)) [ "tags" ] with
+  match Ezjsonm.find_opt (Ezjsonm.value (t.frontmatter)) [ "tags" ] with
   | Some v -> Ezjsonm.get_strings v
   | None -> []
 
 let get_data (t : t) =
-  let markdown = snd t in
   let data : Ezjsonm.value list =
     List.filter_map
       ~f:(fun entry ->
@@ -33,21 +36,21 @@ let get_data (t : t) =
             (* TODO Sexp, ...?? *)
             | _ -> None )
         | _ -> None)
-      markdown
+      t.markdown
   in
   data
 
 let to_json t =
   Ezjsonm.dict
     [
-      ("frontmatter", Ezjsonm.value (fst t));
-      ("content", Ezjsonm.string (Omd.to_text (snd t)));
+      ("frontmatter", Ezjsonm.value (t.frontmatter));
+      ("content", Ezjsonm.string (Omd.to_text (t.markdown)));
       ("data", Ezjsonm.list (fun x -> x) (get_data t)) ;
     ]
 
 let to_string t =
-  let front_matter = Yaml.to_string_exn (Ezjsonm.value (fst t)) in
-  String.concat ~sep:"\n" [ "---"; front_matter; "---"; Omd.to_text (snd t) ]
+  let front_matter = Yaml.to_string_exn (Ezjsonm.value (t.frontmatter)) in
+  String.concat ~sep:"\n" [ "---"; front_matter; "---"; Omd.to_text (t.markdown) ]
 
 (*TODO: Change to Option*)
 let of_string data =
@@ -69,7 +72,7 @@ let of_string data =
       Omd.of_string
         (String.slice data (List.nth_exn indexes 1 + 3) (String.length data))
     in
-    Some (frontmatter, markdown)
+    Some {frontmatter ; markdown}
   else None
 
 let of_string_exn data =
