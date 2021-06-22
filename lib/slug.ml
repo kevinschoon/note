0ff1ecda694ddbe0a0d1902854f765fd6d15e51f
Kevin Schoon <kevinschoon@gmail.com>
2021-06-22 13:01:21 -0400

    use regex for slug parsing

--- lib/slug.ml
+++ lib/slug.ml
@@ -1,6 +1,17 @@
 open Core
 
+let pattern = Re.Pcre.regexp {|(.*)?note-(\d{8})-(\d{1,})(.md)?|}
+
 type t = { path : string; date : Date.t; index : int }
+
+let to_string slug = slug.path
+
+let of_string path =
+  let result = Re.all pattern path |> List.hd_exn in
+  let items = Re.Group.all result |> Array.to_list in
+  let date = Date.parse ~fmt:"%Y%m%d" (List.nth_exn items 2) in
+  let index = int_of_string (List.nth_exn items 3) in
+  { path; date; index }
 
 let shortname t =
   let date_str = Date.format t.date "%Y%m%d" in
@@ -11,37 +22,32 @@ let compare s1 s2 = String.compare s1.path s2.path
 let is_note path =
   Filename.basename path |> String.is_substring ~substring:"note-"
 
-let of_path path =
-  let slug = Filename.chop_extension (Filename.basename path) in
-  let split = String.split ~on:'-' slug in
-  let date = Date.parse ~fmt:"%Y%m%d" (List.nth_exn split 1) in
-  let index = int_of_string (List.nth_exn split 2) in
-  { path; date; index }
-
 let load state_dir =
   state_dir |> Sys.ls_dir |> List.filter ~f:is_note
   |> List.map ~f:(Filename.concat state_dir)
-  |> List.map ~f:of_path
+  |> List.map ~f:of_string
 
-let next state_dir =
-  let slugs = load state_dir in
-  (* find all slugs for today (00:00:00 -> 23:59:59) *)
-  let now = Time.now () in
-  let today = Time.to_date ~zone:Time.Zone.utc now in
-  let tomorrow = Date.add_days today 1 in
-  let filtered =
-    List.filter
-      ~f:(fun slug -> Date.between ~low:today ~high:tomorrow slug.date)
-      slugs
-  in
-  let next_int =
-    List.fold ~init:(-1)
-      ~f:(fun accm slug -> if slug.index > accm then slug.index else accm)
-      filtered
-    + 1
-  in
-  let date_str = Date.format today "%Y%m%d" in
-  let path =
-    Filename.concat state_dir (Core.sprintf "note-%s-%d.md" date_str next_int)
-  in
-  { path; date = today; index = next_int }
+let next ?(last = None) state_dir =
+  let today = Time.now () |> Time.to_date ~zone:Time.Zone.utc in
+  let today_str = Date.format today "%Y%m%d" in
+  match last with
+  | Some last ->
+      let tomorrow = Date.add_days today 1 in
+      if Date.between ~low:today ~high:tomorrow last.date then
+        let index = last.index + 1 in
+        let path =
+          Filename.concat state_dir (sprintf "note-%s-%d.md" today_str index)
+        in
+        { path; date = today; index }
+      else
+        let index = 0 in
+        let path =
+          Filename.concat state_dir (sprintf "note-%s-%d.md" today_str index)
+        in
+        { path; date = today; index }
+  | None ->
+      let index = 0 in
+      let path =
+        Filename.concat state_dir (Core.sprintf "note-%s-%d.md" today_str index)
+      in
+      { path; date = today; index }
--- test/slug_test.ml
+++ test/slug_test.ml
@@ -1,14 +1,22 @@
 open Core
 open Note_lib
 
+let test_slug_of_string () =
+  let input = "/fuu/bar/note-19700101-0.md" in
+  let slug = input |> Slug.of_string in
+  Alcotest.(check string) "conversion" input (slug |> Slug.to_string);
+  let input = "note-19700101-0" in
+  let slug = input |> Slug.of_string in
+  Alcotest.(check string) "bare" input (slug |> Slug.to_string)
+
 let test_slug_shortname () =
-  let s1 = Slug.of_path "fuu/note-19700101-0.md" in
+  let s1 = Slug.of_string "fuu/note-19700101-0.md" in
   Alcotest.(check string) "short name" "note-19700101-0" (Slug.shortname s1)
 
 let test_slug_comparable () =
   let s1, s2 =
-    ( Slug.of_path "fuu/note-19700101-0.md",
-      Slug.of_path "fuu/note-19700101-0.md" )
+    ( Slug.of_string "fuu/note-19700101-0.md",
+      Slug.of_string "fuu/note-19700101-0.md" )
   in
   Alcotest.(check bool) "identical paths" true (Slug.compare s1 s2 = 0)
 
@@ -17,25 +25,24 @@ let test_slug_path () =
   let state_dir = Filename.temp_dir "note-test" "" in
   let slug = Slug.next state_dir in
   let expected =
-    Filename.concat state_dir (sprintf "note-%s-0.md" date_string) 
+    Filename.concat state_dir (sprintf "note-%s-0.md" date_string)
   in
   Alcotest.(check string) "path" expected slug.path
 
 let test_slug_increment () =
-  let state_dir = Filename.temp_dir "note-test" "" in
   let date_string = Time.format (Time.now ()) "%Y%m%d" ~zone:Time.Zone.utc in
-  for i = 0 to 5 do
-    let filename =
-      Filename.concat state_dir (sprintf "note-%s-%d.md" date_string i)
-    in
-    Out_channel.write_all filename ~data:""
-  done;
-  let slug = Slug.next state_dir in
-  Alcotest.(check int) "index" 6 slug.index
+  let slug = Slug.next "/fuu/bar" in
+  let expected = sprintf "/fuu/bar/note-%s-%d.md" date_string 0 in
+  Alcotest.(check string) "check path" expected (slug |> Slug.to_string);
+  let slug = Slug.next ~last:(Some slug) "/fuu/bar" in
+  let expected = sprintf "/fuu/bar/note-%s-%d.md" date_string 1 in
+  Alcotest.(check string) "check path" expected (slug |> Slug.to_string)
 
 let () =
   Alcotest.run "Slug"
     [
+      ( "slug-of-string",
+        [ Alcotest.test_case "ofstring" `Quick test_slug_of_string ] );
       ( "slug-comparable",
         [ Alcotest.test_case "compare" `Quick test_slug_comparable ] );
       ( "slug-shortname",
