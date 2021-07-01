b173c4ba8985df63761bfb705232cb3bb813e142
Kevin Schoon <kevinschoon@gmail.com>
2021-07-01 14:30:27 -0400

    add comments to note.mli

--- lib/note.mli
+++ lib/note.mli
@@ -1,33 +1,50 @@
 module Frontmatter : sig
   type t = { path : string; description : string option; tags : string list }
+  (* metadata in the heading of each markdown file seperated by --- *)
 end
 
 type t
+(* a note represented as a tuple of frontmatter and raw text content *)
 
 type tree = Tree of (t * tree list)
+(* notes arranged in a hierarchical structure *)
 
 val fst : tree -> t
+(* return the top level note of a given tree *)
 
+val to_string : t -> string
+(* return a note with frontmatter and content *)
+
+val of_string : ?path:string option -> string -> t
+(* parse a note with optional frontmatter data *)
+
+val to_json : t -> Ezjsonm.value
+(* get a note as json data with structured data extracted from it *)
+
+val frontmatter : t -> Frontmatter.t
+
+(* get decoded frontmatter structure *)
+val content : t -> string
+(* get the raw text content without frontmatter heading *)
+
+val flatten : ?accm:t list -> tree -> t list
+(* flatten a tree into a list of notes *)
+
+(* 
+ *  high level adapter options for interaction from the CLI
+ *)
 type options = {
   state_dir : string;
   editor : string;
   on_modification : string option;
 }
-
-val to_string : t -> string
-
-val of_string : ?path:string option -> string -> t
-
-val to_json : t -> Ezjsonm.value
-
-val frontmatter : t -> Frontmatter.t
-val content : t -> string
-
-val flatten : ?accm:t list -> tree -> t list
+(* runtime options for interacting with the filesystem and manifest document*)
 
 val load : path:string -> options -> tree
+(* load all notes below the given path *)
 
 val find : path:string -> options -> t option
+(* find a single note *)
 
 val create :
   ?description:string option ->
@@ -36,7 +53,10 @@ val create :
   path:string ->
   options ->
   unit
+(* create a new note opening it in an editor if no content is given *)
 
 val remove : path:string -> options -> unit
+(* remove an existing note *)
 
 val edit : path:string -> options -> unit
+(* edit an existing note opening it in the configured editor *)
